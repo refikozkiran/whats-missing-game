0ea15fbbe01626cbcb9c00ae6f7c369610e5c3b1
refikozkiran <32892980+refikozkiran@users.noreply.github.com>
2026-05-05 20:29:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,12 +14,5 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match("/index.html")))
-  );
+  event.respondWith(fetch(event.request));
 });
